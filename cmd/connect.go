1001f689b7bff248df3ba86a4692add04b5b3094
Joseph Salisbury <salisbury.joseph@gmail.com>
2018-06-01 20:07:16 +0100

    Improves connect behaviour

--- cmd/connect.go
+++ cmd/connect.go
@@ -1,10 +1,12 @@
 package cmd
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"os/exec"
 	"strconv"
+	"time"
 
 	"github.com/JosephSalisbury/vm/provider"
 	"github.com/JosephSalisbury/vm/provider/providerset"
@@ -26,13 +28,24 @@ If --force-mosh or --force-ssh are specified, those methods will be used specifi
 		RunE:         connectRun,
 	}
 
-	forceMosh bool
-	forceSSH  bool
+	forceMosh          bool
+	forceSSH           bool
+	username           string
+	keyPath            string
+	connectTimeout     time.Duration
+	connectRetries     int
+	connectRetryDelay  time.Duration
+	connectSuccessTime time.Duration
 )
 
 func init() {
 	connectCmd.Flags().BoolVar(&forceMosh, "force-mosh", false, "if true, only mosh will be used for the connection")
 	connectCmd.Flags().BoolVar(&forceSSH, "force-ssh", false, "if true, only ssh will be used for the connection")
+	connectCmd.Flags().StringVar(&username, "username", "joe", "username to use for ssh / mosh")
+	connectCmd.Flags().StringVar(&keyPath, "key", "~/.ssh/dev-vm", "path to key to use for ssh")
+	connectCmd.Flags().DurationVar(&connectTimeout, "connect-timeout", 3*time.Second, "connect timeout for ssh")
+	connectCmd.Flags().IntVar(&connectRetries, "connect-retries", 5, "number of retries for ssh")
+	connectCmd.Flags().DurationVar(&connectRetryDelay, "connect-retry-delay", 10*time.Second, "delay between retrying ssh connections")
 
 	rootCmd.AddCommand(connectCmd)
 }
@@ -57,18 +70,15 @@ func connectRun(cmd *cobra.Command, args []string) error {
 	var ip string
 	var port int
 
-	if len(args) == 0 {
+	switch len(args) {
+	case 0:
 		if len(statuses) == 1 {
 			ip = statuses[0].IP
 			port = statuses[0].Port
-		}
-
-		if len(statuses) > 1 {
+		} else {
 			return MultipleVMError
 		}
-	}
-
-	if len(args) == 1 {
+	case 1:
 		for _, status := range statuses {
 			if status.ID == args[0] {
 				ip = status.IP
@@ -77,18 +87,32 @@ func connectRun(cmd *cobra.Command, args []string) error {
 		}
 	}
 
-	logger.Printf("connecting to %s:%v", ip, port)
+	for i := 0; i < connectRetries; i++ {
+		logger.Printf("connecting to %s:%v", ip, port)
 
-	// TODO: Take username from args
-	// TODO: Take key from args
-	// TODO: ignore user keys etc.
-	sshCommand := exec.Command("ssh", ip, "-p", strconv.Itoa(port), "-l", "joe", "-i", "~/.ssh/dev-vm")
-	sshCommand.Stdin = os.Stdin
-	sshCommand.Stdout = os.Stdout
-	sshCommand.Stderr = os.Stderr
+		sshCommand := exec.Command(
+			"ssh",
+			ip,
+			"-p", strconv.Itoa(port),
+			"-l", username,
+			"-i", keyPath,
+			"-o", "ForwardAgent=yes",
+			"-o", "StrictHostKeyChecking=no",
+			"-o", "UserKnownHostsFile=/dev/null",
+			"-o", fmt.Sprintf("ConnectTimeout=%v", connectTimeout.Seconds()),
+		)
+		sshCommand.Stdin = os.Stdin
+		sshCommand.Stdout = os.Stdout
+		sshCommand.Stderr = os.Stderr
 
-	if err := sshCommand.Run(); err != nil {
-		return err
+		err := sshCommand.Run()
+
+		if err != nil {
+			logger.Printf("could not connect, retrying in %v", connectRetryDelay)
+			time.Sleep(connectRetryDelay)
+		} else {
+			return nil
+		}
 	}
 
 	return nil
